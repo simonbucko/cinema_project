2b2144f7c3677a0c694145be4d609b7256c8d46a
vchivu14 <chivu.vlad@yahoo.com>
2021-10-14 11:06:49 +0200

    REST API contract for movies playing

--- src/main/java/cinema/shows/rest/MoviePlayingRESTAPIImp.java
+++ src/main/java/cinema/shows/rest/MoviePlayingRESTAPIImp.java
@@ -2,9 +2,10 @@ package cinema.shows.rest;
 
 import cinema.shows.dtos.MoviePlayingDTO;
 import cinema.shows.services.MoviePlayingServices;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.util.List;
@@ -18,13 +19,18 @@ public class MoviePlayingRESTAPIImp implements MoviePlayingRESTAPI {
         this.moviePlayingServices = moviePlayingServices;
     }
 
-    @Override
-    public ResponseEntity<List<MoviePlayingDTO>> getAllMoviesPlayingForDate(Date date) {
-        return null;
+    @GetMapping("/{date)")
+    public ResponseEntity<List<MoviePlayingDTO>> getAllMoviesPlayingForDate(
+            @RequestParam("date") @DateTimeFormat(pattern="yyyy-MM-dd") Date date) {
+        List<MoviePlayingDTO> moviePlaying = moviePlayingServices.getAllMoviesPlayingForDate(date);
+        return new ResponseEntity<>(moviePlaying, HttpStatus.OK);
     }
 
-    @Override
-    public ResponseEntity<List<MoviePlayingDTO>> getAllMoviesPlayingForDates(Date dateStarts, Date dateEnds) {
-        return null;
+    @GetMapping("/{dateStarts}/{dateEnds}")
+    public ResponseEntity<List<MoviePlayingDTO>> getAllMoviesPlayingForDates(
+            @RequestParam("dateStarts") @DateTimeFormat(pattern="yyyy-MM-dd") Date dateStarts,
+            @RequestParam("dateEnds") @DateTimeFormat(pattern="yyyy-MM-dd") Date dateEnds) {
+        List<MoviePlayingDTO> moviePlaying = moviePlayingServices.getAllMoviesPlayingForDates(dateStarts,dateEnds);
+        return new ResponseEntity<>(moviePlaying, HttpStatus.OK);
     }
 }
