455d8d0495a0472796f6e530ce7568fb4a1ff7c9
SimonBucko <bucino.36@gmail.com>
2021-10-17 15:27:35 +0200

    Unfinish REST endpoint test

--- src/main/java/cinema/shows/dtos/MovieDTO.java
+++ src/main/java/cinema/shows/dtos/MovieDTO.java
@@ -4,10 +4,7 @@ import cinema.shows.entities.Actor;
 import cinema.shows.entities.Movie;
 import cinema.shows.staticCalls.StaticCalls;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,6 +14,7 @@ import java.util.Set;
 @NoArgsConstructor @AllArgsConstructor
 @Getter @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@ToString
 public class MovieDTO {
     private Integer id;
     private String title;
--- src/test/java/cinema/shows/rest/MovieRESTAPIImp.java
+++ src/test/java/cinema/shows/rest/MovieRESTAPIImp.java
@@ -1,6 +1,12 @@
 package cinema.shows.rest;
 
 import cinema.shows.dtos.MovieDTO;
+import cinema.shows.entities.Category;
+import cinema.shows.entities.Movie;
+import cinema.shows.repos.CategoryRepo;
+import cinema.shows.repos.MovieRepo;
+import cinema.shows.testUtils.TestDataMaker;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -17,12 +23,16 @@ import org.springframework.test.context.jdbc.Sql;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+//@AutoConfigureTestDatabase
+@EnableAutoConfiguration
+@SpringBootTest(classes = cinema.shows.CinemaApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MovieRESTAPIImp {
 
     private final String BASE_PATH = "/api/movies";
     private final HttpHeaders headers = new HttpHeaders();
+    private Category category;
+    Movie movie;
 
     @LocalServerPort
     private int port;
@@ -30,30 +40,35 @@ public class MovieRESTAPIImp {
     @Autowired
     TestRestTemplate restTemplate;
 
+    @Autowired
+    MovieRepo movieRepo;
+
+    @Autowired
+    CategoryRepo categoryRepo;
+
     private String makeUrl(String path){
         String pathBuilded = "http://localhost:"+port+path;
         System.out.println(pathBuilded);
         return pathBuilded;
     }
 
+    @BeforeEach
+    public void setupDatabase(){
+        category = TestDataMaker.createCategory(categoryRepo);
+        movie = TestDataMaker.createMovie(movieRepo);
+    }
+
     @Test
-    @Sql("/createMovie.sql")
     void getMovie(){
+        System.out.println(movie);
         HttpEntity<String> entity = new HttpEntity<>(null,headers);
-        ResponseEntity<MovieDTO> response = restTemplate.exchange(makeUrl(BASE_PATH+"/1"), HttpMethod.GET,
+        ResponseEntity<Movie> response = restTemplate.exchange(makeUrl(BASE_PATH+"/"+movie.getId()), HttpMethod.GET,
                 entity,
-                MovieDTO.class
+                Movie.class
         );
+        System.out.println(response.getBody());
         assertEquals("Godfather",response.getBody().getTitle());
     }
-//    void getCustomer() {
-//        HttpEntity<String> entity = new HttpEntity<>(null,headers);
-//        ResponseEntity<CustomerDTO> response = restTemplate.exchange(makeUrl(BASE_PATH+"/100"),
-//                HttpMethod.GET,
-//                entity,
-//                CustomerDTO.class
-//        );
-//
-//        assertEquals("aaa",response.getBody().getFirstName());
-//    }
+
+
 }
